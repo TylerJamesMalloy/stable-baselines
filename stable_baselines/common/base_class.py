99c400d12dd72bf24990cbb19bd2be11679314ec
Kenneth Tang <32535494+ChengYen-Tang@users.noreply.github.com>
2019-12-04 23:46:05 +0800

    fix load model bug: AttributeError: 'NoneType' object has no attribute 'action_space'

--- stable_baselines/common/base_class.py
+++ stable_baselines/common/base_class.py
@@ -758,7 +758,7 @@ class ActorCriticRLModel(BaseRLModel):
         action_masks = []
         if action_mask is not None:
             for env_action_mask in action_mask:
-                action_masks.append(flatten_action_mask(self.env.action_space, env_action_mask))
+                action_masks.append(flatten_action_mask(self.action_space, env_action_mask))
 
         observation = np.array(observation)
         vectorized_env = self._is_vectorized_observation(observation, self.observation_space)
@@ -787,7 +787,7 @@ class ActorCriticRLModel(BaseRLModel):
         action_masks = []
         if action_mask is not None:
             for env_action_mask in action_mask:
-                action_masks.append(flatten_action_mask(self.env.action_space, env_action_mask))
+                action_masks.append(flatten_action_mask(self.action_space, env_action_mask))
 
         observation = np.array(observation)
         vectorized_env = self._is_vectorized_observation(observation, self.observation_space)
